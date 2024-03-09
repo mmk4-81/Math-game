8f7945e6316185065f9529603701b6d994ebe5fa
mmk <mmkkarbalaei@gmail.com>
2024-03-09 22:50:29 +0330

    initial variable

---
+++ app.js
@@ -0,0 +1,9 @@
+let operators = ["+" , "-" , "*" ];
+const startBtn = document.getElementById('start-btn');
+const question  = document.getElementById("questions");
+const controls = document.querySelector(".controls-container");
+const submitBtn = document.getElementById("submit-btn");
+const result = document.getElementById("result");
+const errorMessage = document.getElementById("error-msg");
+let answerValue;
+let operatorQuestuion;
